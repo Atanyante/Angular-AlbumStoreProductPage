edf045704e7d208a2a75fac6e544f61150a8239c
Lin <BLin@dstsystems.com>
2020-05-08 17:09:40 -0400

    take 2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import { Add, Map, Operator } from 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -13,7 +14,7 @@ export class ProductService {
     constructor(private _http: Http){}
 
     getAlbum(id: number): Observable<Album>{
-        return this._http.get<Album>(this._albumUrl).map((response: Album) => response.json());
+        return this._http.get<Album>(this._albumUrl).map((response: Observable<Album>) => response.json());
     }
 
 }
