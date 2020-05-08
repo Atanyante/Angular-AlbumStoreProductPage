75950c07dca4f34929419d7dba1befdbd9b26f87
Lin <BLin@dstsystems.com>
2020-05-08 17:38:20 -0400

    Attempt 1

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Album } from '../album';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-page',
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  albumInfo: Album;
+
+  constructor(private _productService: ProductService) { }
+
+  ngOnInit() {
+      this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+  }
+
+}
